3c5009812106b64148104ebf79ca4684a56a65de
Asherfraz22 <47940404+Asherfraz22@users.noreply.github.com>
2020-10-05 21:06:56 +0500

    Add files via upload

---
+++ otpg/contact-view.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Contact Us | FBVIDEO.GA Facebook Video Downloader</title>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <meta name="Author" content="Asher Fraz">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no ">
+            <!-- Facebook Open Graph Meta Tags -->
+        <meta property="og:type" content="website" />
+        <meta content="FBVIDEO.GA Facebook Video Downloader" property="og:title"/>
+        <meta property="og:site_name" content="FBVIDEO.GA Facebook Video Downloader" />
+        <meta content="https://fbvideo.ga" property="og:url"/>
+        <meta property="og:image" content="https://fbvideo.ga/Favicon.jpg" />
+        <meta property="og:description" content="FBVIDEO.GA - Facebook Video Downloader Online | The Best Free Online Tool to Download Facebook Videos.Simply paste Facebook Video url and click Download button to Save them directly from FBVIDEO to your Computer or Mobile devices.Powered By Blog By Ashirfraz." />
+            <!-- Twitter Open Graph Meta Tags -->
+        <meta property="twitter:title" content="FBVIDEO.GA Facebook Video Downloader" />
+        <meta property="og:url" content="https://fbvideo.ga" />
+        <meta property="twitter:creator" content="@AsherFraz" />
+        <meta property="twitter:site" content="https://fbvideo.ga" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:image" content="https://fbvideo.ga/Favicon.jpg" />
+        <meta property="twitter:description" content="FBVIDEO.GA - Facebook Video Downloader Online | The Best Free Online Tool to Download Facebook Videos.Simply paste Facebook Video url and click Download button to Save them directly from FBVIDEO to your Computer or Mobile devices.Powered By Blog By Ashirfraz." />
+            <!-- Meta Tags -->
+        <meta name="description" content="FBVIDEO.GA - Facebook Video Downloader Online | The Best Free Online Tool to Download Facebook Videos.Simply paste Facebook Video url and click Download button to Save them directly from FBVIDEO to your Computer or Mobile devices.Powered By Blog By Ashirfraz." />
+        <meta name="keywords" content="Facebook Video Downloader,Download Facebook Videos,Facebook Online Downloader , grab facebook online, free facebook video downloader,baixar vídeos do Facebook,baixar facebook,video downloader extension chrome , FACEBOOK VIDEO DOWNLOAD ,fb video downloader , fb Downloader , Facebook Video Downloader Online, Download Facebook Videos and Save them directly from facebook to your computer or mobile for Free without Software. We provide also a Video Downloader Chrome Extension. FBVIDEO Best Facebook Video Downloader. , Download videos to your PC or Phone without registration or restrictions. , Facebook video Downloader Online HD, this is a free fbdown service to Download and Save FB video HD on android, iPhone or desktop from Groups, Pages, Posts link" />
+        
+        <meta property="article:published_time" content="2020-07-22"/>
+        <meta property="article:upgrade_time" content="2020-09-5"/>
+        <meta content='all,index,follow' name='robots'/>
+        <meta http-equiv="refresh" content="900; url=https://fbvideo.ga/" >
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="shortcut icon" type="image/x-icon" href="../Favicon.jpg">
+        <link rel="stylesheet" type="text/css" href="./otpg/contact.css">
+        <meta name="google-site-verification" content="7VKp8aoQ-mW0jtDTFQJ9d5j_Lw5YfO4Dt2Wn8LdmJps" />
+        <script data-ad-client="ca-pub-6418433671263124" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+    </head>
+<body>
+    <div id="loader"></div>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+        <h5 class="my-0 mr-md-auto font-weight-normal"><a href="https://www.fbvideo.ga/" title="GOTO FBVIDEO.GA"> FBVIDEO.GA</a> Facebook Video Downloader</h5>
+        <nav class="my-2 my-md-0 mr-md-3 nav-tabs ">
+            <ul class="nav nav-tabs">
+                <li class="nav-item">
+                    <a class="nav-link active" href="../index.html">Back to Main Page</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../game2048.html" target="_blank" title="Play Games...">Play Games</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+    <div class="form-container ">
+        <h1><strong>Contact US</strong></h1>
+        <form name="frmContact" id="" frmContact"" method="post"
+            action="" enctype="multipart/form-data"
+            onsubmit="return validateContactForm()">
+
+            <div class="input-row">
+                <label style="padding-top: 20px;"><strong>Name</strong></label> <span
+                    id="userName-info" class="info"></span><br /> <input
+                    type="text" class="input-field" name="userName"
+                    id="userName" placeholder="Enter Name" />
+            </div>
+            <div class="input-row">
+                <label><strong>Email</strong></label> <span id="userEmail-info"
+                    class="info"></span><br /> <input type="text"
+                    class="input-field" name="userEmail" id="userEmail" placeholder="Enter Email" />
+            </div>
+            <div class="input-row">
+                <label><strong>Subject</strong></label> <span id="subject-info"
+                    class="info"></span><br /> <input type="text"
+                    class="input-field" name="subject" id="subject" placeholder="Type Message Subject" />
+            </div>
+            <div class="input-row">
+                <label><strong>Message</strong></label> <span id="userMessage-info"
+                    class="info"></span><br />
+                <textarea name="content" id="content"
+                    class="input-field" cols="60" rows="6" placeholder="Write Your Message in Decent Manner !
+                    Also Put Your Country and Gender Details "></textarea>
+            </div>
+            <div>
+                <input type="submit" name="send" class="btn-submit btn-primary"
+                    value="Send" />
+
+                <div id="statusMessage"> 
+                        <?php
+                        if (! empty($message)) {
+                            ?>
+                            <p class='<?php echo $type; ?>Message'><?php echo $message; ?></p>
+                        <?php
+                        }
+                        ?>
+                    </div>
+            </div>
+        </form>
+    </div>
+<footer class="footer mt-auto py-3">
+                <div class="container-fuild text-center"><hr>
+                    <span class="text-muted"><strong>Made with ❤️ </strong><a href="https://www.ashirfraz.ml" target="_blank" title="Blog By Asherfraz." ><strong>Blog By Asherfraz.</strong></a><br>___________________________________________________________<br>Copyright&#169; <strong>2020.</strong> All Rights Reserved.
+                </span><hr>
+            </div>
+</footer>
+<!-- JavaScript Code -->
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"
+        type="text/javascript"></script>
+    <script type="text/javascript">
+        function validateContactForm() {
+            var valid = true;
+
+            $(".info").html("");
+            $(".input-field").css('border', '#e0dfdf 1px solid');
+            var userName = $("#userName").val();
+            var userEmail = $("#userEmail").val();
+            var subject = $("#subject").val();
+            var content = $("#content").val();
+            
+            if (userName == "") {
+                $("#userName-info").html("Required.");
+                $("#userName").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+            if (userEmail == "") {
+                $("#userEmail-info").html("Required.");
+                $("#userEmail").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+            if (!userEmail.match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/))
+            {
+                $("#userEmail-info").html("Invalid Email Address.");
+                $("#userEmail").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+
+            if (subject == "") {
+                $("#subject-info").html("Required.");
+                $("#subject").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+            if (content == "") {
+                $("#userMessage-info").html("Required.");
+                $("#content").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+            return valid;
+        }
+</script>
+</body>
+</html>
